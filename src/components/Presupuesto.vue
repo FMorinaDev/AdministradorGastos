06b46a6fb5cd4ebf632738f16b057fea031455ef
Franco Morina <francomorina@outlook.com>
2023-07-29 10:23:37 -0300

    comienzo

---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <form >
+        <div class="campo">
+            <label for="">Definir presupuesto</label>
+            <input type="text" id="nuevo-presupuesto" class="nuevo-presupuesto" placeholder="Añade tu presupuesto">
+        </div>
+        <input type="submit" value="Definir Presupuesto">
+    </form>
+</template>
+<style lang="sass" scoped>
+
+</style>
